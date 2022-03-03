4f47a84159ce6565016320a7c8397d5a6d29b798
benny <benny08118@gmail.com>
2022-03-03 11:53:14 +0800

    add templates in first_project

--- first_project/first_Flask.py
+++ first_project/first_Flask.py
@@ -1,12 +1,27 @@
-from flask import Flask, app
+from flask import Flask, render_template, url_for, redirect
 
 
 app=Flask(__name__)
 
-@app.route("/")
 
-def test():
-    return "Hi"
+
+# @app.route("/<name>")
+# def test(name=None):
+#     if name==None:
+#         return "Hi !!"
+#     return "Hello " + name+' !!'
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route('/second')
+def test_s():
+	return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
